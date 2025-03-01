1c0b580e5490fe36efe7dfa7f720c07b5991fa62
AbhimanTIGER <abhimansisira473@gmail.com>
2025-02-28 22:19:41 -0800

    pasidu

---
+++ Js/script.js
@@ -0,0 +1,30 @@
+        // EmailJS Setup
+        (function() {
+            emailjs.init("ZpKDpI8nIH_D5YqCn");  // Replace YOUR_USER_ID with your EmailJS user ID
+        })();
+
+        // Form Submit Event Listener
+        const form = document.getElementById("contact-form");
+
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            // Form Fields
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const phone = document.getElementById("phone").value;
+            const message = document.getElementById("message").value;
+
+            // Send Email using EmailJS
+            emailjs.send("service_ldh54m4", "template_75l8i3d", {
+                name: name,
+                email: email,
+                phone: phone,
+                message: message
+            })
+            .then(function(response) {
+                alert("Message sent successfully!");
+            }, function(error) {
+                alert("Failed to send message. Please try again.");
+            });
+        });
