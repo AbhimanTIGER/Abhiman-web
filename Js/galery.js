a77b971944427e241e5db4d93de9066d3337962f
AbhimanTIGER <abhimansisira473@gmail.com>
2025-03-02 02:11:59 -0800

    I'm pasidu

---
+++ Js/galery.js
@@ -0,0 +1,36 @@
+document.getElementById("skilo").style.display = "none";
+function buton1() {
+    document.getElementById("dimo1").innerHTML = "About me";
+    document.getElementById("imgg").style.display = "block";
+    document.getElementById("note").style.display = "block";
+    document.getElementById("skilo").style.display = "none";
+    document.getElementById("lon2").style.backgroundColor = " #0ef";
+    
+    
+}
+function buton2() {
+    document.getElementById("dimo1").innerHTML = "My abilities";
+    document.getElementById("imgg").style.display = "none";
+    document.getElementById("note").style.display = "none";
+    document.getElementById("skilo").style.display = "block";
+    document.getElementById("lon2").style.backgroundColor = "rgb(65, 170, 255)";
+
+
+
+}
+function buton3() {
+    document.getElementById("dimo1").innerHTML = "My project";
+    document.getElementById("imgg").style.display = "none";
+    document.getElementById("note").style.display = "none";
+    document.getElementById("skilo").style.display = "none";
+    document.getElementById("lon2").style.backgroundColor = "rgb(65, 170, 255)";
+
+}
+function buton4() {
+    document.getElementById("dimo1").innerHTML = "Services";
+    document.getElementById("imgg").style.display = "none";
+    document.getElementById("note").style.display = "none";
+    document.getElementById("skilo").style.display = "none";
+    document.getElementById("lon2").style.backgroundColor = "rgb(65, 170, 255)";
+
+}
